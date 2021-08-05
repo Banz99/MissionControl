6354e21d93e6328ba74c8230a82be05d90c8d3cd
Banz99 <42299919+Banz99@users.noreply.github.com>
2021-08-05 22:21:32 +0200

    Implement per controller profiles (using their bt address) and a global kill switch to disable profiles entirely

--- mc_mitm/source/mcmitm_config.hpp
+++ mc_mitm/source/mcmitm_config.hpp
@@ -22,6 +22,10 @@ namespace ams::mitm {
             char host_name[0x20];
             bluetooth::Address host_address;
         } bluetooth;
+
+        struct{
+            bool disable_custom_profiles;
+        } general;
     };
 
     struct ControllerProfileConfig{
